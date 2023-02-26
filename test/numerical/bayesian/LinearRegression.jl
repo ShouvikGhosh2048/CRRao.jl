08668b490a30222b4029e0b4883c17dd5ce849bb
ShouvikGhosh2048 <shouvikg.cal@gmail.com>
2023-02-26 14:43:32 +0530

    test

--- test/numerical/bayesian/LinearRegression.jl
+++ test/numerical/bayesian/LinearRegression.jl
@@ -8,16 +8,29 @@ tests = [
     (Prior_HorseShoe(), 20.042984550677183),
 ]
 
-for (prior, test_mean) in tests
-    CRRao.set_rng(StableRNG(123))
-    model = fit(@formula(MPG ~ HP + WT + Gear), mtcars, LinearRegression(), prior)
+CRRao.set_rng(StableRNG(123))
+model = fit(@formula(MPG ~ HP + WT + Gear), mtcars, LinearRegression(), Prior_Ridge(), 0.01, 10000)
+@test mean(predict(model, mtcars)) ≈ 20.080877893580514
 
-    @test mean(predict(model, mtcars)) ≈ test_mean
-end
+CRRao.set_rng(StableRNG(123))
+model = fit(@formula(MPG ~ HP + WT + Gear), mtcars, LinearRegression(), Prior_Laplace(), 0.01, 10000)
+@test mean(predict(model, mtcars)) ≈ 20.070783434589128
+
+CRRao.set_rng(StableRNG(123))
+model = fit(@formula(MPG ~ HP + WT + Gear), mtcars, LinearRegression(), Prior_Cauchy(), 10000)
+@test mean(predict(model, mtcars)) ≈ 20.019759144845644
+
+CRRao.set_rng(StableRNG(123))
+model = fit(@formula(MPG ~ HP + WT + Gear), mtcars, LinearRegression(), Prior_TDist(), 2.0, 10000)
+@test mean(predict(model, mtcars)) ≈ 20.042614331921428
+
+CRRao.set_rng(StableRNG(123))
+model = fit(@formula(MPG ~ HP + WT + Gear), mtcars, LinearRegression(), Prior_HorseShoe(), 10000)
+@test mean(predict(model, mtcars)) ≈ 20.042984550677183
 
 gauss_test = 20.0796026428345
 
 CRRao.set_rng(StableRNG(123))
-model = fit(@formula(MPG ~ HP + WT + Gear), mtcars, LinearRegression(), Prior_Gauss(), 30.0, [0.0,-3.0,1.0], 1000)
+model = fit(@formula(MPG ~ HP + WT + Gear), mtcars, LinearRegression(), Prior_Gauss(), 30.0, [0.0,-3.0,1.0], 10000)
 
 @test mean(predict(model, mtcars)) ≈ gauss_test
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,49 +6,49 @@ CRRao.setprogress!(false)
 CRRao.set_rng(StableRNG(123))
 
 @testset "CRRao.jl" begin
-    @testset "Basic Tests" begin
-        @testset "Linear Regression" begin
-            include("basic/LinearRegression.jl")
-        end
+    # @testset "Basic Tests" begin
+    #     @testset "Linear Regression" begin
+    #         include("basic/LinearRegression.jl")
+    #     end
 
-        @testset "Logistic Regression" begin
-            include("basic/LogisticRegression.jl")
-        end
+    #     @testset "Logistic Regression" begin
+    #         include("basic/LogisticRegression.jl")
+    #     end
 
-        @testset "Poisson Regression" begin
-            include("basic/PoissonRegression.jl")
-        end
+    #     @testset "Poisson Regression" begin
+    #         include("basic/PoissonRegression.jl")
+    #     end
 
-        @testset "Negative Binomial Regression" begin
-            include("basic/NegBinomialRegression.jl")
-        end
-    end
+    #     @testset "Negative Binomial Regression" begin
+    #         include("basic/NegBinomialRegression.jl")
+    #     end
+    # end
 
     @testset "Numerical Tests" begin
-        @testset "Random Number Generator" begin
-            include("numerical/RandomNumberGenerator.jl")
-        end
+        # @testset "Random Number Generator" begin
+        #     include("numerical/RandomNumberGenerator.jl")
+        # end
 
-        @testset "Frequentist" begin
-            include("numerical/frequentist/tests.jl")
-        end
+        # @testset "Frequentist" begin
+        #     include("numerical/frequentist/tests.jl")
+        # end
 
         @testset "Bayesian" begin
             @testset "Linear Regression" begin
                 include("numerical/bayesian/LinearRegression.jl")
             end
 
-            @testset "Logistic Regression" begin
-                include("numerical/bayesian/LogisticRegression.jl")
-            end
+            # @testset "Logistic Regression" begin
+            #     include("numerical/bayesian/LogisticRegression.jl")
+            # end
 
-            @testset "Poisson Regression" begin
-                include("numerical/bayesian/PoissonRegression.jl")
-            end
+            # @testset "Poisson Regression" begin
+            #     include("numerical/bayesian/PoissonRegression.jl")
+            # end
 
-            @testset "Negative Binomial Regression" begin
-                include("numerical/bayesian/NegBinomialRegression.jl")
-            end
+            # @testset "Negative Binomial Regression" begin
+            #     include("numerical/bayesian/NegBinomialRegression.jl")
+            # end
         end
     end
 end
